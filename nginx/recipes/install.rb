1970e3f1950677f1a7a65b180e90453e3ba64b25
ozawaseijiro <seijiro4751@gmail.com>
2015-07-15 13:33:09 +0900

    set default nginx conf

--- nginx/recipes/install.rb
+++ nginx/recipes/install.rb
@@ -7,3 +7,11 @@ service 'nginx' do
   action [:enable, :start]
 end
 
+template 'default.conf' do
+  path "/etc/nginx/conf.d/default.conf"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :restart, "service[nginx]"
+end
+
